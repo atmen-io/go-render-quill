1cfb1992e95239f1a4e50932984206b7dd99865a
dchenk <dchenk@users.noreply.github.com>
2017-12-31 12:51:46 -0800

    added testing

--- render_test.go
+++ render_test.go
@@ -1,7 +1,9 @@
 package quill
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
@@ -82,4 +84,35 @@ func TestExtractString(t *testing.T) {
 	if extractString(false) != "n" {
 		t.Errorf("failed bool false extract")
 	}
+}
+
+func TestOps1(t *testing.T) {
+	if err := testPair("ops1.json", "ops1.html"); err != nil {
+		t.Errorf("%s", err)
+	}
+}
+
+func TestEmpty(t *testing.T) {
+	if err := testPair("empty.json", "empty.html"); err != nil {
+		t.Errorf("%s", err)
+	}
+}
+
+func testPair(opsFile, htmlFile string) error {
+	opsArr, err := ioutil.ReadFile("./tests/" + opsFile)
+	if err != nil {
+		return fmt.Errorf("could not read %s; %s\n", opsFile, err)
+	}
+	desired, err := ioutil.ReadFile("./tests/" + htmlFile)
+	if err != nil {
+		return fmt.Errorf("could not read %s; %s\n", htmlFile, err)
+	}
+	got, err := Render(opsArr, nil)
+	if err != nil {
+		return fmt.Errorf("error rendering; %s\n", err)
+	}
+	if !bytes.Equal(desired, got) {
+		return fmt.Errorf("bad rendering; wanted: \n%s\n got: \n%s\n", desired, got)
+	}
+	return nil
 }
